b9fcf7dbb73b67b1ce9a77e401cff8f65df047da
RajendraGund <rajendragund2015@gmail.com>
2021-09-21 03:45:50 +0530

    Added : Welcome message for the User

---
+++ src/main/java/com/bridgelabz/lambda/UserRegistrationMain.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.lambda;
+
+public class UserRegistrationMain {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the User Registration code using Lambda");
+    }
+}
