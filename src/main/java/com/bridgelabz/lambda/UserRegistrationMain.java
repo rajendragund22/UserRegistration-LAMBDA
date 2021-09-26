075e58f15f428ba97b05a928c38887003227296d
RajendraGund <rajendragund2015@gmail.com>
2021-09-26 14:58:59 +0530

    Added : Program for the check first name is valid or not

--- src/main/java/com/bridgelabz/lambda/UserRegistrationMain.java
+++
@@ -1,7 +0,0 @@
-package com.bridgelabz.lambda;
-
-public class UserRegistrationMain {
-    public static void main(String[] args) {
-        System.out.println("Welcome to the User Registration code using Lambda");
-    }
-}
